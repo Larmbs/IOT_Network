695f4258a8a4950c9a87792e94e0dfb6037735fe
Liam <liamisrael200@gmail.com>
2024-05-01 19:00:16 -0400

    Adding previous projects here

--- devices/models.py
+++ devices/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Device(models.Model):
     device_id = models.CharField(max_length=100)
     device_name = models.CharField(max_length=50)
+    device_description = models.CharField(max_length=150)
     
     def __str__(self):
         return self.device_name + " " + self.device_id
--- devices/views.py
+++ devices/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Device
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def tracker(request):
+    return render(request, 'tracker/tracker.html', {"device_list":Device.objects.all()})
+
+def tracker_device_inspect(request):
+    device_id = request.Get.get()
+    
+
+def credits(request):
+    return render(request, 'credits.html', {"device_list":Device.objects.all()})
